40927da9d21f6bc00224597ad2f301f14b3f2098
rahul2-17 <dr24csb1a12@student.nitw.ac.in>
2025-02-25 13:59:53 +0530

    Update movielogic.js

--- movielogic.js
+++ movielogic.js
@@ -1,10 +1,24 @@
 let addMovie=document.querySelector("#addMovie");
+let display=document.querySelector("#display");
 
 let allMovies=[];
 
 addMovie.addEventListener("click",()=>{
     let newMovie=prompt("Enter the name of the movie");
     allMovies.push(newMovie);
+    displayMovies();
 })
 
 console.log(allMovies);
+
+
+function displayMovies(){
+    display.textContent="";
+    allMovies.forEach((movie)=>{
+        let movieItem=document.createElement("div");
+        movieItem.textContent=`${movie}`;
+        movieItem.style.padding="10px";
+        movieItem.style.margin="5px";
+        display.appendChild(movieItem);
+    });
+}
